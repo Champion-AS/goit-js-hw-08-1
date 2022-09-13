f90d19fcfb2d0ef813d0d9ecf81e0ff0584aad42
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-13 20:48:25 +0300

    1+

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryMarkupGeneration(galleryItems);
+
+function galleryMarkupGeneration(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}"/>
+         </a>`
+    )
+    .join('');
+}
+
+var $gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
