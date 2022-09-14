d8e4ef35c5ec2d3c84fff857abb587362fb18d77
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-14 15:46:52 +0300

    3+

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import Throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('[name="email"]');
+const message = form.querySelector('[name="message"]');
+
+const localForm = "feedback-form-state";
+
+form.addEventListener('input', Throttle(storageFormData, 500));
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault();
+    // console.log(event.currenTarget.elements.value)
+    const saveData = JSON.parse(localStorage.getItem(localForm));
+
+    console.dir(saveData);
+    
+        
+    localStorage.removeItem(localForm);
+    event.currentTarget.reset();
+
+}
+
+function storageFormData(event) {
+    const formValue = { email: '', massage: '' };
+
+   if (localStorage.getItem(localForm)) {
+        Object.assign(formValue, JSON.parse(localStorage.getItem(localForm)))
+   }
+    
+    formValue[event.target.name] = event.target.value;
+ //console.log(formValue)
+    
+    localStorage.setItem(localForm, JSON.stringify(formValue))
+    
+}
